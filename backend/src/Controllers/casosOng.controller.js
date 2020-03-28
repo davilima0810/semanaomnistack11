62bb11a9aa1ce7e89bf8e756b30893e332ec2ea0
davilima0810 <davicruzcontato@gmail.com>
2020-03-28 15:40:10 -0300

    Front-end do app

--- backend/src/Controllers/casosOng.controller.js
+++ backend/src/Controllers/casosOng.controller.js
@@ -3,8 +3,6 @@ const connection = require('../database/connection')//para op com bd
 module.exports = {
   async index(req, res){
     const ong_id = req.headers.authorization;
-
-    console.log(ong_id)
     const caso = await connection('casos').where('ong_id', ong_id).select('*');
 
     return res.json(caso)
